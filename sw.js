c93e05db35d4d6cd6803d4837d331de06cdd2c24
rk7sensi-1 <jg2814313@gmail.com>
2026-02-03 20:49:05 -0300

    Add service worker for caching assets
    Implement service worker for caching and offline support.

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'clash-royale-v1';
+const urlsToCache = [
+  './rk7s-sensi.html',
+  './manifest.json',
+  './clash-royale.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Update Service Worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
